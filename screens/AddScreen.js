391166ecbf86790e1f03430b6fa3d32bee3e35e7
Homit Dalia <homitdalia59@gmail.com>
2023-02-25 19:03:13 +0530

    Image Upload Working

--- screens/AddScreen.js
+++ screens/AddScreen.js
@@ -4,6 +4,63 @@ import Ionicons from 'react-native-vector-icons/Ionicons'
 import DocumentPicker from 'react-native-document-picker'
 import storage from '@react-native-firebase/storage';
 import getPath from '@flyerhq/react-native-android-uri-path'
+import RNEncryptionModule from "@dhairyasharma/react-native-encryption"
+
+const plainText = "Hello World"
+const password = "1234"
+
+async function encryptFile(file) {
+
+  // RNEncryptionModule.encryptText(
+  //   plainText,
+  //   password
+  // ).then((res) => {
+  //   if (res.status == "success") {
+  //     console.log("Successfully Encrypted the File")
+  //     console.log(res.encryptedText)
+  //     decryptFile(res)
+  //   } else {
+  //     Alert.alert("Error", res);
+  //   }
+  // }).catch((err) => {
+  //   console.log(err);
+  // });
+
+  const newPath = getPath(file.uri)
+  RNEncryptionModule.encryptFile(
+    newPath,
+    outputEncryptedFilePath,
+    password
+  ).then((res) => {
+    if (res.status == "success") {
+      console.log("success", res)
+    } else {
+      console.log("error", res);
+    }
+  }).catch((err) => {
+    console.log(err);
+  });
+
+}
+
+async function decryptFile(file) {
+
+  RNEncryptionModule.decryptText(
+    file.encryptedText,
+    password,
+    file.iv,
+    file.salt).then((res) => {
+      if (res.status == "success") {
+        console.log("Successfully decrpted the file")
+        console.log(res.decryptedText)
+      } else {
+        Alert.alert("Error", res);
+      }
+    }).catch((err) => {
+      console.log(err);
+    });
+}
+
 
 const AddScreen = () => {
 
@@ -16,12 +73,12 @@ const AddScreen = () => {
 
       console.log(file);
       uploadImage(file)
-      
+
     } catch (error) {
-      if(DocumentPicker.isCancel(error))
-          console.log("User cancelled Document picker", error)
+      if (DocumentPicker.isCancel(error))
+        console.log("User cancelled Document picker", error)
       else
-          console.log(error)
+        console.log(error)
     }
   };
 
@@ -35,7 +92,7 @@ const AddScreen = () => {
 
     const task = reference.putFile(pathToFile);
     task.on('state_changed', taskSnapshot => {
-      console.log(`${taskSnapshot.bytesTransferred/1000000}MB transferred out of ${taskSnapshot.totalBytes/1000000}MB`);
+      console.log(`${taskSnapshot.bytesTransferred / 1000000}MB transferred out of ${taskSnapshot.totalBytes / 1000000}MB`);
     });
 
     task.then(() => {
@@ -52,7 +109,10 @@ const AddScreen = () => {
         <Ionicons name='cloud-upload' color='#088821' size={40} />
       </TouchableOpacity>
 
-
+      <TouchableOpacity style={styles.uploadIcon}
+        onPress={encryptFile}>
+        <Ionicons name='hardware-chip-outline' color='#696969' size={80} />
+      </TouchableOpacity>
     </View>
   )
 }
